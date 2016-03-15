406c8cd2f3ceae01ffdf4fdd00823b3a339d2f49
Mert Saygı <mertsaygi@gmail.com>
2016-03-15 09:14:59 +0200

    Subprojects are created

--- hita/settings.py
+++ hita/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'management',
+    'api',
+    'rest_framework_swagger',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
